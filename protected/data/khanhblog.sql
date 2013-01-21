b992e72c4523f451d70a12b04c414eae397c37f2
1rhino <buikhanh.bk@gmail.com>
2013-01-22 00:52:57 +0700

    create model, crud

---
+++ protected/data/khanhblog.sql
@@ -0,0 +1,144 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : buikhanh
+Source Server Version : 50516
+Source Host           : localhost:3306
+Source Database       : khanhblog
+
+Target Server Type    : MYSQL
+Target Server Version : 50516
+File Encoding         : 65001
+
+Date: 2013-01-22 00:52:05
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `tbl_article`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_article`;
+CREATE TABLE `tbl_article` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `intro` text NOT NULL,
+  `content` text NOT NULL,
+  `tags` text,
+  `status` int(11) NOT NULL,
+  `category` int(11) NOT NULL,
+  `create_time` int(11) DEFAULT NULL,
+  `update_time` int(11) DEFAULT NULL,
+  `author_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tbl_article
+-- ----------------------------
+INSERT INTO `tbl_article` VALUES ('1', 'Chia tay YuMe', 'Hết tuần này là chia tay YuMe.', 'Hết tuần này là chia tay YuMe.\r\nChuẩn bị tìm việc mới', 'Yume, chia-tay', '1', '1', null, null, '1');
+
+-- ----------------------------
+-- Table structure for `tbl_category`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_category`;
+CREATE TABLE `tbl_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `parent` int(11) NOT NULL,
+  `intro` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tbl_category
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `tbl_comment`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_comment`;
+CREATE TABLE `tbl_comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  `author` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `url` varchar(128) DEFAULT NULL,
+  `status` int(11) NOT NULL,
+  `create_time` int(11) DEFAULT NULL,
+  `article_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tbl_comment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `tbl_status`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_status`;
+CREATE TABLE `tbl_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `code` int(11) NOT NULL,
+  `type` varchar(128) NOT NULL,
+  `position` int(11) NOT NULL,
+  `intro` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tbl_status
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `tbl_tag`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_tag`;
+CREATE TABLE `tbl_tag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `frequency` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tbl_tag
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `tbl_url`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_url`;
+CREATE TABLE `tbl_url` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `intro` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tbl_url
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `tbl_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_user`;
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `salt` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `phone` decimal(10,0) DEFAULT NULL,
+  `address` varchar(128) DEFAULT NULL,
+  `profile` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tbl_user
+-- ----------------------------
+INSERT INTO `tbl_user` VALUES ('1', 'admin', 'e4c8294ee7f4d8d57bb1ddd639555090', '77612c8f7d163d961ea9d2d0906d799e', 'buikhanh.bk@gmail.com', '986205854', 'Gò Vấp, HCMC', 'Chase excellent. Success will follow.');
